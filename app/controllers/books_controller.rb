c380b4f6a6c20c9cce08da8031b40ef576ac4a93
Haruibos <sunada.381724@gmail.com>
2021-08-29 14:40:52 +0000

    修正版

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,54 +1,52 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
     @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+      
+    end
+    
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
-    @book = Book.new
-  end
-
- def create
-   # １. データを新規登録するためのインスタンス作成
-   @book = Book.new(book_params)
-    if @book.save
-      flash[:notice] = "Book was successfully created."
-        redirect_to "/books/#{@book.id}"
-    else
-      @books = Book.all
-      render :index
-    end
- end
-
   def edit
-     @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "Book was successfully update."
-      redirect_to book_path(@book)
+      flash[:not] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
     else
-      # @book = Book.find(params[:id])
-      render action: :edit
+      render :edit
+    end
   end
-end
 
   def destroy
-    flash[:notice] = "Book was successfully destroyed"
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:not] = "Book was successfully destroyed."
+      redirect_to books_path
+    else
+      render :index
+    end
+    
   end
 
   private
-  # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
